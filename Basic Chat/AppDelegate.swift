0d5788dae80f1d0c6fdb6fd40f30974ac58f100a
TrevKnows <Trevor.Beaton11@gmail.com>
2017-07-18 12:04:53 -0400

    working

--- Basic Chat/AppDelegate.swift
+++ Basic Chat/AppDelegate.swift
@@ -38,7 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.changes made
     }
 
 
